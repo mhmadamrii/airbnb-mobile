d2e60611f720a3b832654439c23e6502a17cc985
mhmadamrii <interceptorghost4@gmail.com>
2024-01-07 17:59:44 +0700

    prettify everything

--- hooks/useWarmUpBrowser.ts
+++ hooks/useWarmUpBrowser.ts
@@ -2,10 +2,10 @@ import { useEffect } from 'react';
 import * as WebBrowser from 'expo-web-browser';
 
 export const useWarmUpBrowser = () => {
-    useEffect(() => {
-        void WebBrowser.warmUpAsync();
-        return () => {
-            void WebBrowser.coolDownAsync();
-        };
-    }, []);
+  useEffect(() => {
+    void WebBrowser.warmUpAsync();
+    return () => {
+      void WebBrowser.coolDownAsync();
+    };
+  }, []);
 };
